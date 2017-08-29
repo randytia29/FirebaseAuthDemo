95f0d97ee61da677272431940e4bed545ccec3cc
Randytia <Randytia.akbar@gmail.com>
2017-08-29 16:27:05 +0700

    update

---
+++ app/src/main/java/com/example/randytia/firebaseauthdemo/UserInformation.java
@@ -0,0 +1,20 @@
+package com.example.randytia.firebaseauthdemo;
+
+/**
+ * Created by Randytia on 22/08/2017.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
